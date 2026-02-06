77ed7c16eb05d4a9a858f9eadf383d43c9fbc863
João Augusto Perin <joaoaugustoperin@gmail.com>
2026-02-06 15:46:46 -0300

    Expose cre-cli derivation

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,9 @@
 { pkgs }:
 let
   portmaster = import ./portmaster { inherit pkgs; };
-  cre-cli = import ./cre-cli { inherit pkgs; };
 in
 {
   inherit (portmaster) portmaster;
-  inherit (cre-cli) cre-cli;
+  # Directly expose the cre-cli derivation
+  cre-cli = import ./cre-cli { inherit pkgs; };
 }
