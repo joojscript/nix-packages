be4917fb2748d3ea2a65d52bb70d1d7b0c676bb7
João Augusto Perin <joaoaugustoperin@gmail.com>
2026-05-05 10:15:42 -0300

    Update balena-etcher

--- packages/balena-etcher/default.nix
+++ packages/balena-etcher/default.nix
@@ -25,6 +25,7 @@
 , mesa
 , nss
 , nspr
+, atk
 , gcc
 , makeWrapper
 }:
@@ -65,6 +66,7 @@ let
     mesa
     nss
     nspr
+    atk
     gcc.cc.lib
   ];
 in
@@ -97,6 +99,7 @@ stdenv.mkDerivation {
     mesa
     nss
     nspr
+    atk
     gcc.cc.lib
   ];
 
